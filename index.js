2bc17ec1afe32be5fd9553684e2b6739d2ac79e3
janmmeier <74722696+janmmeier@users.noreply.github.com>
2021-05-26 20:15:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-// axios.get('https://extrafabulouscomics.com/').then((response)=> {
-//     const $ = cheerio.load(response.data);
-//     console.log($('#comic img').attr('src'));
-// });
-
 (async ()=> {
     for (let i = 28; i >18; i--){
         let response = await axios.get('https://extrafabulouscomics.com/comic/1-' + i);
